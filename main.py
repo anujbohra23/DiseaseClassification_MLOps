696eb50ba7e9c79196c735ab01d31c47b03f56ab
anujbohra23 <anujbohra10@gmail.com>
2024-12-03 11:19:50 +0530

    Made changes in the data ingestion stage, resolved path problem of config and src

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from CNN_Classifier import logger
+from CNN_Classifier.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
 
-logger.info("hey there, this is custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
